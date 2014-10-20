272b5c4f6667e8946667641303e24413aee249ff
Ethan J. Johnson <ethanjohnson89@gmail.com>
2014-10-20 16:16:26 -0400

    Fix input glitch on level 2 caused by Network OnOff Foobar Thing
    I knew that hack was going to come back and bite me...

--- Lockdown/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ Lockdown/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -16,7 +16,7 @@ function Start()
 	// assemblies, and NetworkManager in the main assembly. The purpose of this object is
 	// to signal whether we're in "networked/online" or "offline" mode.
 	var networkingOn = false;
-	if(netOnOffFoobarThing.activeInHierarchy)
+	if(netOnOffFoobarThing != null && netOnOffFoobarThing.activeInHierarchy)
 		networkingOn = true;
 	
 	playerInputSuffix = " P1"; // default to player 1
