a6435177a44a4be29dfae681822bcc83e201141a
Nate Higgins <natehiggins1@gmail.com>
2014-11-13 02:21:33 -0500

    Level 2 clean up

---
+++ Lockdown/Assets/Platformside.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platformside : MonoBehaviour {
+	Vector3 startpos;
+
+	// Use this for initialization
+	void Start () {
+
+		Physics.IgnoreLayerCollision(11, 12, true);
+		startpos = gameObject.transform.position;
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.localPosition = new Vector3(0.0f,-200.5f,0.0f);
+	}
+}
