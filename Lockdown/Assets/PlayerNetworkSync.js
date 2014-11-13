a516b20fbfb26db6a593b40cceb852bb9d478273
Ethan J. Johnson <ethanjohnson89@gmail.com>
2014-11-12 23:09:48 -0500

    Sync player velocity over network so animations are synced

---
+++ Lockdown/Assets/PlayerNetworkSync.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+private var charMotor : CharacterMotor;
+
+function Start () {
+	charMotor = gameObject.GetComponent("CharacterMotor") as CharacterMotor;
+}
+
+function Update () {
+
+}
+
+function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo) : void
+{
+	var myVel : Vector3 = Vector3.zero;
+	if(stream.isWriting) // send data
+	{
+		myVel = charMotor.movement.velocity;
+		stream.Serialize(myVel);
+	}
+	else // receive data
+	{
+		stream.Serialize(myVel);
+		charMotor.movement.velocity = myVel;
+	}
+}
