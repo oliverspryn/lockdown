5a4fd22b5a0632bb9e3bfc8714afc0bede131a2f
weaverreaper <weaverem1@gcc.edu>
2014-11-14 01:59:52 -0500

    Added GUI for items held.

--- Lockdown/Assets/PlayerInfo.cs
+++ Lockdown/Assets/PlayerInfo.cs
@@ -5,13 +5,24 @@ public class PlayerInfo : MonoBehaviour {
 
 	public int items = 0;
 
+	GameObject itemsDisplay;
+	string layerString;
 	// Use this for initialization
 	void Start () {
-	
+		
+		if(gameObject.tag == "Player 1")
+			itemsDisplay = Instantiate(Resources.Load ("KeyPicP") as GameObject) as GameObject;
+		else if(gameObject.tag == "Player 2")
+			itemsDisplay = Instantiate(Resources.Load ("HammerPicP") as GameObject) as GameObject;
+		else if(gameObject.tag == "Player 3")
+			itemsDisplay = Instantiate(Resources.Load ("FloppyPicP") as GameObject) as GameObject;
+
+		itemsDisplay.guiText.text = "" + items;
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		itemsDisplay.guiText.text = "" + items;
 	}
 }
