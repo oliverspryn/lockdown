f0154e20f38308d9b9eb84f7c20db33d60e36308
Nate Higgins <natehiggins1@gmail.com>
2014-12-15 20:37:11 -0500

    Level 2 done.

---
+++ Lockdown/Assets/Audioline10.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Audioline10 : MonoBehaviour {
+	int stage;
+	Dictionary<string, Transform> dict = new Dictionary<string, Transform>();
+	// Use this for initialization
+	void Start () {
+		stage = -1;
+		foreach(Transform t in transform)
+		{
+			dict.Add(t.name, t);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameObject.audio.isPlaying && stage == 0) {
+			stage = 1;
+			Transform child = dict["Carl11"];
+			child.audio.Play();
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(stage == -1)
+		{
+			gameObject.audio.Play ();
+			stage = 0;
+		}
+	}
+}
---
+++ Lockdown/Assets/Audioline7.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Audioline7 : MonoBehaviour {
+	int stage;
+	Dictionary<string, Transform> dict = new Dictionary<string, Transform>();
+	// Use this for initialization
+	void Start () {
+		stage = -1;
+		foreach(Transform t in transform)
+		{
+			dict.Add(t.name, t);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameObject.audio.isPlaying && stage == 0) {
+			stage = 1;
+			Transform child = dict["Carl2"];
+			child.audio.Play();
+		}
+		if (stage == 1) {
+			Transform child = dict["Carl2"];
+			if(!child.audio.isPlaying)
+			{
+				stage = 2;
+				child = dict["Vincent3"];
+				child.audio.Play();
+			}
+			
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(stage == -1)
+		{
+			gameObject.audio.Play ();
+			stage = 0;
+		}
+	}
+}
